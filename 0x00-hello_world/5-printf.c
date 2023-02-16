a927a22936d57abdd684a169acc1d85512f75004
Victor Bunyali Midega <vbm7752@gmail.com>
2023-02-16 13:16:48 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/***
+ * main - A program that print a ine using the printf function
+ * Return:0 (Succesful!)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
